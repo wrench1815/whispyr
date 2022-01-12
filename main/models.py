613ca5c0e9999287de825d3f871b5c3fe9d2a1e5
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-12 19:04:39 +0530

    add basic Model strictire for Secret Messages

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SecretMessage(models.Model):
+    """Model definition for SecretMessage."""
+
+    message = models.TextField()
+
+    class Meta:
+        """Meta definition for SecretMessage."""
+
+        verbose_name = 'Secret Message'
+        verbose_name_plural = 'Secret Messages'
+
+    def __str__(self):
+        """Unicode representation of SecretMessage."""
+        return 'Model Name: Secret Message'
