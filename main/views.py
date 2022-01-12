b8aac647fe67b3857d809a6faeb760f357e72344
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-12 19:05:23 +0530

    add views for site Pages
    add routes for site pages
    
    rsgister app urls in Global url file

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'main/home.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
