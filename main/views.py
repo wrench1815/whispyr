2e44caf8791b927c5bbdbfa7e36d92eb46bb749f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-21 20:02:21 +0530

    add view to take message and save in db

--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import SecretMessage
 # Create your views here.
 
 
 def home(request):
     if request.method == 'POST':
-        pass
+        message = request.POST['SecetMessage']
+
+        SecretMessage.objects.create(message=message)
+        return redirect('/')
+
     return render(request, 'main/home.html')
 
 
