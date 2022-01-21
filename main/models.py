ee76c8088fcb82af1dbeafa6be16a7a4bf6acd14
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-21 20:01:43 +0530

    add slug field on model add method to generate a random slug

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+import random, string
 
+from django.db.models.fields import SlugField
 # Create your models here.
 
 
@@ -7,6 +9,7 @@ class SecretMessage(models.Model):
     """Model definition for SecretMessage."""
 
     message = models.TextField()
+    slug = models.SlugField(max_length=255)
 
     class Meta:
         """Meta definition for SecretMessage."""
@@ -17,3 +20,16 @@ class SecretMessage(models.Model):
     def __str__(self):
         """Unicode representation of SecretMessage."""
         return 'Model Name: Secret Message'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slug_generator()
+            super(SecretMessage, self).save(*args, **kwargs)
+
+
+def slug_generator():
+    '''Generates random slug'''
+    return ''.join(
+        random.choices(string.ascii_lowercase + string.digits +
+                       string.ascii_uppercase,
+                       k=30))
